ffa3ce22975f4bab267b2913c2dbf1098cc499fd
Eduardas Kazakas <eduardas.kazakas@gmail.com>
2018-03-16 15:40:18 +0100

    Initial changes

---
+++ src/main/java/com/sensoric/sensor/SensorService.java
@@ -0,0 +1,11 @@
+package com.sensoric.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorService {
+	public static void main(String[] args) {
+		SpringApplication.run(SensorService.class, args);
+	}
+}
