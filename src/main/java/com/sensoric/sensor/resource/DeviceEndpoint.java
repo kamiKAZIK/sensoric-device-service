e55c988bd766f8cbf750e3ef30ce6bfa7e21a7fa
Eduardas Kazakas <eduardas.kazakas@gmail.com>
2018-08-18 00:46:19 +0200

    Moved controller

--- src/main/java/com/sensoric/sensor/resource/DeviceEndpoint.java
+++ src/main/java/com/sensoric/sensor/resource/DeviceEndpoint.java
@@ -4,10 +4,12 @@ import com.sensoric.sensor.domain.model.Device;
 import com.sensoric.sensor.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 
+@RequestMapping(name = "/devices")
 @RestController
 public class DeviceEndpoint {
     private final DeviceService service;
@@ -17,7 +19,7 @@ public class DeviceEndpoint {
         this.service = service;
     }
 
-    @GetMapping(name = "/devices")
+    @GetMapping
     public Set<Device> fetchDevices() {
         return service.fetchDevices();
     }
